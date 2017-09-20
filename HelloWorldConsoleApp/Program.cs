8856e443d2d543842e17b33fdca65616089850c1
STONEGATEMTG\ecaldwell <elvin.caldwell@stonegatemtg.com>
2017-09-20 10:24:47 -0400

    New App for test

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HelloWorldConsoleApp
 {
     class Program
     {
+        private static bool cancel = false;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Application has started. Ctrl-C to end");
+
+            string helloworld = "This is a Hello World program";
+                        //write Hello world to console
+                      Console.WriteLine(helloworld);
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+           
+            var autoResetEvent = new AutoResetEvent(false);
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                // cancel the  the program 
+                eventArgs.Cancel = true;
+                autoResetEvent.Set();
+            };
+
+            // waiting for ctrl-C
+            autoResetEvent.WaitOne();
+            cancel = true;
+            Console.WriteLine("Now shutting down");
+
+            
         }
+
+        
+        
     }
+    
 }
